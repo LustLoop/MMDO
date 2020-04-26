2fcd4add6d6aabbc0658d1ef6904669b3ec3cf61
Volodymyr <volodymyrye@tahometer.com>
2020-04-26 19:20:27 +0300

    fixed dichotomy

--- Function.cs
+++ Function.cs
@@ -4,12 +4,11 @@ namespace MMDO4
 {
     public class Function
     {
-
         public int Nf;
         public double getResult(double x)
         {
             Nf++;
-            return 4 * Math.Pow((3 - x), 2/3) + 2 * Math.Pow(x, 3);
+            return 4 * Math.Pow((3 - x), 2.0 / 3.0) + 2 * Math.Pow(x, 3);
         }
     }
 }
